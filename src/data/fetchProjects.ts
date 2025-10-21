6fc8ed8f5706cf87ee0f1623a3a1576975b9184e
naufalfaisa <naufalfaisa386@gmail.com>
2025-10-21 15:58:01 +0700

    update

--- src/data/fetchProjects.ts
+++ src/data/fetchProjects.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 export type Project = {
   id: number;
   title: string;
@@ -6,7 +8,8 @@ export type Project = {
   // demoUrl: string | null;
   // stars: number;
   // forks: number;
-  // language: string;
+  language: string;
+  color: string;
   // topics: string[];
   image: string;
 };
@@ -19,15 +22,34 @@ const repoLinks = [
 ];
 
 const coverMap: Record<string, string> = {
-  "naufalfaisa/amly": "https://i.imgur.com/NL4O93e.jpeg",
+  "naufalfaisa/amly": "/images/amly.webp",
+  "naufalfaisa/amdl": "/images/amdl.webp",
+  "naufalfaisa/portfolio": "/images/portfolio.webp"
   // repo cover image
 };
 
 export async function fetchProjects(): Promise<Project[]> {
+  const cachePath = "./src/data/projects-cache.json";
+  const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN;
+
+  if (fs.existsSync(cachePath)) {
+    const cached = JSON.parse(fs.readFileSync(cachePath, "utf-8"));
+    return cached;
+  }
+
+  const colorRes = await fetch("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json");
+  const colors = await colorRes.json();
+
+  const headers = GITHUB_TOKEN
+    ? { Authorization: `token ${GITHUB_TOKEN}`, Accept: "application/vnd.gtihub.v3+json"}
+    : {};
+
   const projects: Project[] = await Promise.all(
     repoLinks.map(async (fullName) => {
       const res = await fetch(`https://api.github.com/repos/${fullName}`);
       const data = await res.json();
+      const lang = data.language || "Unknown";
+      const color = colors[lang]?.color || "#999999";
 
       return {
         id: data.id,
@@ -38,11 +60,13 @@ export async function fetchProjects(): Promise<Project[]> {
         // stars: data.stargazers_count,
         // forks: data.forks_count,
         language: data.language || "Unknown",
+        color,
         // topics: data.topics || [],
         image: coverMap[fullName] || "https://placehold.jp/1280x720.png",
       };
     })
   );
 
+  fs.writeFileSync(cachePath, JSON.stringify(projects, null, 2));
   return projects;
 }
